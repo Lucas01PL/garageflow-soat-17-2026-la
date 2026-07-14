a41e391a444ce0c3af5177180e331654e4912537
Lucas Pereira <lucas01_ce@hotmail.com>
2026-07-13 21:07:13 -0300

    Initial commit.

---
+++ src/main/java/br/com/fiap/tech/challeng/garageflow_soat_17_2026_la/GarageflowSoat172026LaApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.tech.challeng.garageflow_soat_17_2026_la;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarageflowSoat172026LaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarageflowSoat172026LaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/tech/challeng/garageflow_soat_17_2026_la/GarageflowSoat172026LaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.tech.challeng.garageflow_soat_17_2026_la;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarageflowSoat172026LaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
