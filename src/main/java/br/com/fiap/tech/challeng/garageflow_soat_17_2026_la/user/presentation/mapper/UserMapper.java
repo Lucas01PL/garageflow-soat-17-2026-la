4c9d51fcd201cdf4d80b718920226b75b2730776
Lucas Pereira <lucas01_ce@hotmail.com>
2026-07-21 20:45:10 -0300

    feat: implement user presentation layer

---
+++ src/main/java/br/com/fiap/tech/challeng/garageflow_soat_17_2026_la/user/presentation/mapper/UserMapper.java
@@ -0,0 +1,26 @@
+package br.com.fiap.tech.challeng.garageflow_soat_17_2026_la.user.presentation.mapper;
+
+import br.com.fiap.tech.challeng.garageflow_soat_17_2026_la.user.domain.model.User;
+import br.com.fiap.tech.challeng.garageflow_soat_17_2026_la.user.presentation.dto.response.CreateUserRequestDTO;
+import br.com.fiap.tech.challeng.garageflow_soat_17_2026_la.user.presentation.dto.request.UserResponseDTO;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+
+    public User toModel(CreateUserRequestDTO dto) {
+        if (dto == null) return null;
+        return new User(dto.getFullName(), dto.getEmail(), dto.getPassword(), dto.getStatus());
+    }
+
+    public UserResponseDTO toResponse(User model) {
+        if (model == null) return null;
+        return new UserResponseDTO(
+                model.getId(),
+                model.getFullName(),
+                model.getEmail(),
+                model.getStatus()
+        );
+    }
+}
+
