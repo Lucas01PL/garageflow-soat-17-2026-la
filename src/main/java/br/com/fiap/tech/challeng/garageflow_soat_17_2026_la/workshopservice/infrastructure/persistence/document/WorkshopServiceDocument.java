a9159646edba18b987c9cf6fd1d2422f22f46478
Lucas Pereira <lucas01_ce@hotmail.com>
2026-07-21 21:42:18 -0300

    refactor: simplify document by using Lombok annotations

--- src/main/java/br/com/fiap/tech/challeng/garageflow_soat_17_2026_la/workshopservice/infrastructure/persistence/document/WorkshopServiceDocument.java
+++ src/main/java/br/com/fiap/tech/challeng/garageflow_soat_17_2026_la/workshopservice/infrastructure/persistence/document/WorkshopServiceDocument.java
@@ -1,5 +1,7 @@
 package br.com.fiap.tech.challeng.garageflow_soat_17_2026_la.workshopservice.infrastructure.persistence.document;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import jakarta.validation.constraints.NotBlank;
@@ -7,6 +9,8 @@ import jakarta.validation.constraints.Positive;
 
 import java.math.BigDecimal;
 
+@NoArgsConstructor
+@Data
 @Document(collection = "services")
 public class WorkshopServiceDocument {
 
@@ -19,45 +23,10 @@ public class WorkshopServiceDocument {
     @Positive(message = "Value must be greater than zero")
     private BigDecimal value;
 
-
-    public WorkshopServiceDocument() { }
-
     public WorkshopServiceDocument(String description, BigDecimal value) {
         this.description = description;
         this.value = value;
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public BigDecimal getValue() {
-        return value;
-    }
-
-    public void setValue(BigDecimal value) {
-        this.value = value;
-    }
-
-    @Override
-    public String toString() {
-        return "WorkshopService{" +
-                "id='" + id + '\'' +
-                ", description='" + description + '\'' +
-                ", value=" + value +
-                '}';
-    }
 }
 
