281307724a0a7cf5610d92ab5f0a67efae9d2434
Lucas Pereira <lucas01_ce@hotmail.com>
2026-07-27 21:27:09 -0300

    refactor: simplify document by using Lombok annotations

--- src/main/java/br/com/fiap/tech/challeng/garageflow_soat_17_2026_la/user/infrastructure/persistence/document/UserDocument.java
+++ src/main/java/br/com/fiap/tech/challeng/garageflow_soat_17_2026_la/user/infrastructure/persistence/document/UserDocument.java
@@ -1,10 +1,17 @@
 package br.com.fiap.tech.challeng.garageflow_soat_17_2026_la.user.infrastructure.persistence.document;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
+@ToString
+@NoArgsConstructor
+@Data
 @Document(collection = "users")
 public class UserDocument {
 
@@ -24,63 +31,5 @@ public class UserDocument {
     @NotBlank(message = "Status cannot be blank")
     private String status;
 
-    public UserDocument() { }
-
-    public UserDocument(String fullName, String email, String password, String status) {
-        this.fullName = fullName;
-        this.email = email;
-        this.password = password;
-        this.status = status;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    @Override
-    public String toString() {
-        return "UserDocument{" +
-                "id='" + id + '\'' +
-                ", fullName='" + fullName + '\'' +
-                ", email='" + email + '\'' +
-                ", status='" + status + '\'' +
-                '}';
-    }
 }
 
