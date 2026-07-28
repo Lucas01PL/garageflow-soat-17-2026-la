4d18b12eff3da6fa710d26baf60ab694f0b48d1a
Lucas Pereira <lucas01_ce@hotmail.com>
2026-07-27 21:27:26 -0300

    refactor: simplify model by using Lombok annotations

--- src/main/java/br/com/fiap/tech/challeng/garageflow_soat_17_2026_la/user/domain/model/User.java
+++ src/main/java/br/com/fiap/tech/challeng/garageflow_soat_17_2026_la/user/domain/model/User.java
@@ -1,7 +1,11 @@
 package br.com.fiap.tech.challeng.garageflow_soat_17_2026_la.user.domain.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 
+@NoArgsConstructor
+@Data
 public class User {
 
     @Id
@@ -11,8 +15,6 @@ public class User {
     private String password;
     private String status;
 
-    public User() { }
-
     public User(String fullName, String email, String password, String status) {
         this.fullName = fullName;
         this.email = email;
@@ -20,54 +22,4 @@ public class User {
         this.status = status;
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id='" + id + '\'' +
-                ", fullName='" + fullName + '\'' +
-                ", email='" + email + '\'' +
-                ", status='" + status + '\'' +
-                '}';
-    }
 }
-
