49c7ccedc86a68f6744be068bf41f36985c58d2b
Lucas Pereira <lucas01_ce@hotmail.com>
2026-08-08 15:07:30 -0300

    feat: implement RepairOrderDocument and related classes for repair order management

---
+++ src/main/java/br/com/fiap/tech/challeng/garageflow_soat_17_2026_la/repairorder/infrastructure/persistence/document/RepairOrderCustomerDocument.java
@@ -0,0 +1,7 @@
+package br.com.fiap.tech.challeng.garageflow_soat_17_2026_la.repairorder.infrastructure.persistence.document;
+
+public class RepairOrderCustomerDocument {
+    private String documentNumber;
+    private String fullName;
+    private String phone;
+}
---
+++ src/main/java/br/com/fiap/tech/challeng/garageflow_soat_17_2026_la/repairorder/infrastructure/persistence/document/RepairOrderDocument.java
@@ -0,0 +1,58 @@
+package br.com.fiap.tech.challeng.garageflow_soat_17_2026_la.repairorder.infrastructure.persistence.document;
+
+import br.com.fiap.tech.challeng.garageflow_soat_17_2026_la.repairorder.domain.type.RepairOrderStatus;
+import jakarta.validation.constraints.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@NoArgsConstructor
+@Data
+@Document(collection = "repair_orders")
+public class RepairOrderDocument {
+
+    @Id
+    private String id;
+
+    @Positive(message = "Number must be greater than zero")
+    private String number;
+
+    @NotBlank(message = "Status cannot be blank")
+    private RepairOrderStatus status;
+
+    private LocalDateTime initDate;
+
+    private LocalDateTime finishDate;
+
+    @Positive(message = "Total services must be greater than zero")
+    private BigDecimal totalServices;
+
+    @Positive(message = "Total parts must be greater than zero")
+    private BigDecimal totalParts;
+
+    @Positive(message = "Total must be greater than zero")
+    private BigDecimal total;
+
+    @NotBlank(message = "Workshop Services cannot be blank")
+    private List<RepairOrderWorkshopServiceDocument> workshopServices;
+
+    @NotBlank(message = "Parts cannot be blank")
+    private List<RepairOrderPartDocument> parts;
+
+    @NotBlank(message = "Customer cannot be blank")
+    private RepairOrderCustomerDocument customer;
+
+    @NotBlank(message = "User ID cannot be blank")
+    private String userId;
+
+    private LocalDateTime createdDate;
+
+    private LocalDateTime updatedDate;
+
+}
+
