6f08caf9731b12dffd9313dcfcb004cdc6ee7c7d
Lucas Pereira <lucas01_ce@hotmail.com>
2026-08-18 21:58:25 -0300

    refactor: simplify exception messages in InvalidFieldValueException and RequiredObjectException

--- src/main/java/br/com/fiap/tech/challeng/garageflow_soat_17_2026_la/shared/exception/InvalidFieldValueException.java
+++ src/main/java/br/com/fiap/tech/challeng/garageflow_soat_17_2026_la/shared/exception/InvalidFieldValueException.java
@@ -3,7 +3,7 @@ package br.com.fiap.tech.challeng.garageflow_soat_17_2026_la.shared.exception;
 public class InvalidFieldValueException extends BusinessException {
 
     public InvalidFieldValueException(String fieldName, String message) {
-        super(String.format("Field '%s' is invalid. '%s'", fieldName, message));
+        super(String.format("Field '%s' is invalid. %s", fieldName, message));
     }
 
 }
--- src/main/java/br/com/fiap/tech/challeng/garageflow_soat_17_2026_la/shared/exception/RequiredObjectException.java
+++ src/main/java/br/com/fiap/tech/challeng/garageflow_soat_17_2026_la/shared/exception/RequiredObjectException.java
@@ -3,7 +3,7 @@ package br.com.fiap.tech.challeng.garageflow_soat_17_2026_la.shared.exception;
 public class RequiredObjectException extends BusinessException {
 
     public RequiredObjectException(String objectName) {
-        super(String.format("'%s' cannot be null.", objectName));
+        super(String.format("%s cannot be null.", objectName));
     }
 
 }
