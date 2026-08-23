64e876a510714ba95f9027a1c3f3f439784dda55
Lucas Pereira <lucas01_ce@hotmail.com>
2026-08-23 18:18:23 -0300

    refactor: rename SecurityIntegrationTest class package to shared.config.security

--- src/test/java/br/com/fiap/tech/challeng/garageflow_soat_17_2026_la/shared/config/security/SecurityIntegrationTest.java
+++ src/test/java/br/com/fiap/tech/challeng/garageflow_soat_17_2026_la/shared/config/security/SecurityIntegrationTest.java
@@ -1,4 +1,4 @@
-package br.com.fiap.tech.challeng.garageflow_soat_17_2026_la.configuration.security;
+package br.com.fiap.tech.challeng.garageflow_soat_17_2026_la.shared.config.security;
 
 import br.com.fiap.tech.challeng.garageflow_soat_17_2026_la.config.MongoTestContainerConfiguration;
 import br.com.fiap.tech.challeng.garageflow_soat_17_2026_la.workshopservice.presentation.dto.CreateWorkshopServiceRequestDTO;
