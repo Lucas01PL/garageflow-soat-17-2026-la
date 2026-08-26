23d7f76bd7592b58ac5ebfe1d9680ed898ab8879
Lucas Pereira <lucas01_ce@hotmail.com>
2026-08-26 19:48:50 -0300

    refactor: update WorkshopServiceController endpoint path for improved clarity

--- src/main/java/br/com/fiap/tech/challeng/garageflow_soat_17_2026_la/workshopservice/presentation/controller/WorkshopServiceController.java
+++ src/main/java/br/com/fiap/tech/challeng/garageflow_soat_17_2026_la/workshopservice/presentation/controller/WorkshopServiceController.java
@@ -20,7 +20,7 @@ import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @RestController
-@RequestMapping("/workshopservice")
+@RequestMapping("/workshop-service")
 public class WorkshopServiceController {
 
     private CreateWorkshopServiceUseCase createUseCase;
