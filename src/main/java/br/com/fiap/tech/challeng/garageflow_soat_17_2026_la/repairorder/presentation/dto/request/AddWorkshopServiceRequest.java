0811c5b20cfb8a84a0b0d68e0b6c5450b029ebfb
Lucas Pereira <lucas01_ce@hotmail.com>
2026-08-27 19:09:59 -0300

    refactor: add validation annotations to AddWorkshopServiceRequest fields

--- src/main/java/br/com/fiap/tech/challeng/garageflow_soat_17_2026_la/repairorder/presentation/dto/request/AddWorkshopServiceRequest.java
+++ src/main/java/br/com/fiap/tech/challeng/garageflow_soat_17_2026_la/repairorder/presentation/dto/request/AddWorkshopServiceRequest.java
@@ -2,6 +2,8 @@ package br.com.fiap.tech.challeng.garageflow_soat_17_2026_la.repairorder.present
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 @Data
@@ -9,6 +11,8 @@ public class AddWorkshopServiceRequest {
     @NotBlank(message = "Workshop Service ID cannot be blank")
     private String workshopServiceId;
 
+    @NotNull(message = "Quantity cannot be null")
+    @Positive(message = "Quantity must be greater than zero")
     @Min(value = 1, message = "Quantity must be a positive integer")
     private Integer quantity;
 }
