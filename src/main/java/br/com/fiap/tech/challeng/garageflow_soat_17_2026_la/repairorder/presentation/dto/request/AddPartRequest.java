221b0c4e2c02d15f5c67ad20d0e763d62d50b38f
Lucas Pereira <lucas01_ce@hotmail.com>
2026-08-27 19:09:35 -0300

    refactor: add validation annotations to AddPartRequest fields

--- src/main/java/br/com/fiap/tech/challeng/garageflow_soat_17_2026_la/repairorder/presentation/dto/request/AddPartRequest.java
+++ src/main/java/br/com/fiap/tech/challeng/garageflow_soat_17_2026_la/repairorder/presentation/dto/request/AddPartRequest.java
@@ -2,6 +2,8 @@ package br.com.fiap.tech.challeng.garageflow_soat_17_2026_la.repairorder.present
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 @Data
@@ -10,6 +12,8 @@ public class AddPartRequest {
     @NotBlank(message = "Part ID cannot be blank")
     private String partId;
 
+    @NotNull(message = "Quantity cannot be null")
+    @Positive(message = "Quantity must be greater than zero")
     @Min(value = 1, message = "Quantity must be a positive integer")
     private Integer quantity;
 }
