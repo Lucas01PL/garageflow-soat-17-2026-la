de3101a98ce0f4365d23da0a421a2dad5953ff4e
Lucas Pereira <lucas01_ce@hotmail.com>
2026-08-26 22:16:56 -0300

    refactor: replace IllegalArgumentException with RequiredFieldException in DeleteWorkshopServiceUseCase

--- src/main/java/br/com/fiap/tech/challeng/garageflow_soat_17_2026_la/workshopservice/application/usecase/DeleteWorkshopServiceUseCase.java
+++ src/main/java/br/com/fiap/tech/challeng/garageflow_soat_17_2026_la/workshopservice/application/usecase/DeleteWorkshopServiceUseCase.java
@@ -1,5 +1,6 @@
 package br.com.fiap.tech.challeng.garageflow_soat_17_2026_la.workshopservice.application.usecase;
 
+import br.com.fiap.tech.challeng.garageflow_soat_17_2026_la.shared.exception.RequiredFieldException;
 import br.com.fiap.tech.challeng.garageflow_soat_17_2026_la.workshopservice.domain.repository.WorkshopServiceRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,7 +13,7 @@ public class DeleteWorkshopServiceUseCase {
 
     public boolean execute(String id) {
         if (id == null || id.isBlank()) {
-            throw new IllegalArgumentException("Service ID cannot be empty");
+            throw new RequiredFieldException("id");
         }
         if (!repository.existsById(id)) {
             return false;
