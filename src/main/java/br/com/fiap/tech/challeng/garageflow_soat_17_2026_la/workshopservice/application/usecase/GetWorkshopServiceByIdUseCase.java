0823e74d5fe2e8d6266c8354849d9ba6fdf40590
Lucas Pereira <lucas01_ce@hotmail.com>
2026-08-26 21:38:07 -0300

    refactor: replace IllegalArgumentException with RequiredFieldException for service ID validation in GetWorkshopServiceByIdUseCase

--- src/main/java/br/com/fiap/tech/challeng/garageflow_soat_17_2026_la/workshopservice/application/usecase/GetWorkshopServiceByIdUseCase.java
+++ src/main/java/br/com/fiap/tech/challeng/garageflow_soat_17_2026_la/workshopservice/application/usecase/GetWorkshopServiceByIdUseCase.java
@@ -1,5 +1,6 @@
 package br.com.fiap.tech.challeng.garageflow_soat_17_2026_la.workshopservice.application.usecase;
 
+import br.com.fiap.tech.challeng.garageflow_soat_17_2026_la.shared.exception.RequiredFieldException;
 import br.com.fiap.tech.challeng.garageflow_soat_17_2026_la.workshopservice.domain.model.WorkshopService;
 import br.com.fiap.tech.challeng.garageflow_soat_17_2026_la.workshopservice.domain.repository.WorkshopServiceRepository;
 import lombok.AllArgsConstructor;
@@ -15,7 +16,7 @@ public class GetWorkshopServiceByIdUseCase {
 
     public Optional<WorkshopService> execute(String id) {
         if (id == null || id.isBlank()) {
-            throw new IllegalArgumentException("Service ID cannot be empty");
+            throw new RequiredFieldException("id");
         }
         return repository.findById(id);
     }
