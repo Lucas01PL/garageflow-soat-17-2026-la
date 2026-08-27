f90bb3c31327abf2db14eb8f368170d32b98696a
Lucas Pereira <lucas01_ce@hotmail.com>
2026-08-27 19:03:55 -0300

    refactor: add @NotBlank annotation to VehicleRequest plate field for validation

--- src/main/java/br/com/fiap/tech/challeng/garageflow_soat_17_2026_la/vehicle/presentation/dto/VehicleRequest.java
+++ src/main/java/br/com/fiap/tech/challeng/garageflow_soat_17_2026_la/vehicle/presentation/dto/VehicleRequest.java
@@ -1,6 +1,9 @@
 package br.com.fiap.tech.challeng.garageflow_soat_17_2026_la.vehicle.presentation.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record VehicleRequest(
+        @NotBlank(message = "Plate is required")
         String plate,
         String brand,
         String model,
