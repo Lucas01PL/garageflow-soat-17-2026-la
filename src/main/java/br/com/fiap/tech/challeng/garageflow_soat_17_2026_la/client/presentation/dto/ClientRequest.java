bb8a488f46d913293acccc084b81c4a76f2bc36c
Lucas Pereira <lucas01_ce@hotmail.com>
2026-08-27 19:01:53 -0300

    refactor: add validation annotations to ClientRequest fields

--- src/main/java/br/com/fiap/tech/challeng/garageflow_soat_17_2026_la/client/presentation/dto/ClientRequest.java
+++ src/main/java/br/com/fiap/tech/challeng/garageflow_soat_17_2026_la/client/presentation/dto/ClientRequest.java
@@ -1,9 +1,16 @@
 package br.com.fiap.tech.challeng.garageflow_soat_17_2026_la.client.presentation.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record ClientRequest(
+        @NotBlank(message = "Name is required")
         String name,
+
+        @NotBlank(message = "Document is required")
         String document,
         String phone,
+
+        @NotBlank(message = "Email is required")
         String email,
         String address
 ) {
