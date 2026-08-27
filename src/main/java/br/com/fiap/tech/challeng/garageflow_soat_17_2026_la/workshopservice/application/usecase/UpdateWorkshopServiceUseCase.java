f07f9ef13fc6bc528e9b475e8a77ca1b63163179
Lucas Pereira <lucas01_ce@hotmail.com>
2026-08-27 18:49:05 -0300

    refactor: throw ResourceNotFoundException when updating non-existent workshop service

--- src/main/java/br/com/fiap/tech/challeng/garageflow_soat_17_2026_la/workshopservice/application/usecase/UpdateWorkshopServiceUseCase.java
+++ src/main/java/br/com/fiap/tech/challeng/garageflow_soat_17_2026_la/workshopservice/application/usecase/UpdateWorkshopServiceUseCase.java
@@ -1,6 +1,7 @@
 package br.com.fiap.tech.challeng.garageflow_soat_17_2026_la.workshopservice.application.usecase;
 
 import br.com.fiap.tech.challeng.garageflow_soat_17_2026_la.shared.exception.RequiredFieldException;
+import br.com.fiap.tech.challeng.garageflow_soat_17_2026_la.shared.exception.ResourceNotFoundException;
 import br.com.fiap.tech.challeng.garageflow_soat_17_2026_la.workshopservice.domain.model.WorkshopService;
 import br.com.fiap.tech.challeng.garageflow_soat_17_2026_la.workshopservice.domain.repository.WorkshopServiceRepository;
 import lombok.AllArgsConstructor;
@@ -21,7 +22,7 @@ public class UpdateWorkshopServiceUseCase {
         }
         Optional<WorkshopService> existing = repository.findById(id);
         if (existing.isEmpty()) {
-            return Optional.empty();
+            throw new ResourceNotFoundException("WorkshopService", "id", id);
         }
         WorkshopService svc = existing.get();
         if (update.getDescription() != null && !update.getDescription().isBlank()) {
